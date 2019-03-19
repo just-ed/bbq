136fcdeafdff15f16cd248a66a532507ea9ea6bb
just-ed <ed_tomeyan@mail.ru>
2019-03-19 21:25:38 +0300

    authorization added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
         keys: [:password, :password_confirmation, :current_password]
     )
   end
+
+  helper_method :current_user_can_edit?
+
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
+  end
 end

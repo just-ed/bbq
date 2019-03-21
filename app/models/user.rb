139e82f2a14e8c007950de5fb5fa603945fc104b
just-ed <ed_tomeyan@mail.ru>
2019-03-21 14:24:53 +0300

    subscriptions added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   before_validation :set_name, on: :create
 
+  after_commit :link_subscriptions, on: :create
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,10 +10,15 @@ class User < ApplicationRecord
 
   has_many :events
   has_many :comments, dependent: :destroy
+  has_many :subscriptions
 
   validates :name, presence: true, length: {maximum: 35}
 
   private
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: email).update_all(user_id: id)
+  end
 
   def set_name
     self.name = "Комрад №#{rand(999)}" if name.blank?

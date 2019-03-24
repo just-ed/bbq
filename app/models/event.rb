72526793793e112c4c3c62eb93ba9284d83344af
just-ed <ed_tomeyan@mail.ru>
2019-03-24 22:51:58 +0300

    delete_all dependency for photos

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :subscriptions
   has_many :subscribers, through: :subscriptions, source: :user
-  has_many :photos
+  has_many :photos, dependent: :delete_all
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true

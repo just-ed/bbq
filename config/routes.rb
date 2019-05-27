21abb217161fe121b7b4cc7603d16e55ba8083e7
just-ed <ed_tomeyan@mai.ru>
2019-05-27 23:09:36 +0300

    devise routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'registrations',
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root "events#index"
 
